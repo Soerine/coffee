24b31a2fb38df9f8615a6797a88be74ac00a94ae
mahlaclothing <soerinefaber@hotmail.com>
2019-02-08 13:27:38 +0100

    n

--- server/services/forside_service.js
+++ server/services/forside_service.js
@@ -17,5 +17,23 @@ module.exports = {
         })
     },
 
+    kaffe_detalje: (kaffe_id) => {
+        return new Promise((resolve, reject) => {
+            let db = mysql.connect();
+            db.execute(`SELECT kaffe_id, kaffe_navn, kaffe_pris, kaffe_billede FROM kaffe
+            
+            WHERE kaffe_id = ?`, [kaffe_id], (err, rows) => {
+                    if (err) {
+                        reject(err.message)
+                    } else {
+                        console.log(rows[0])
+                        resolve(rows[0])
+                    }
+
+                })
+            db.end();
+        })
+    }
+
 
 }
